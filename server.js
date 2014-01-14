26d69d42741d52cf87f5744c0aacc58552aacbfb
Yrjö Kari-Koskinen <ykk@peruna.fi>
2014-01-14 21:18:14 +0200

    read configuration from parameters. handle empty data

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 var http = require("http");
+var https = require("https");
 var url = require("url");
 var _ = require("underscore");
 
-var dataSource = "http://localhost:8000/data.json";
-var cachePeriod = 5; // 5 sec
+var cachePeriod = 20; // 20 sec
 var cache = {
 	valid: 0,
-	data: {}
+	data: ""
 };
+
+
+function parseArgs(args) {
+	var confArgs = args.slice(2);
+	if (confArgs.length < 3) {
+		console.log("Usage: " + args.slice(0,2).join(" ") +  " port dataSource authToken");
+		process.exit(1);
+	}
+	return {port: confArgs[0], dataSource: confArgs[1], authToken: confArgs[2] };
+}
 
 function currentTimeInSeconds() {
 	return new Date().getTime() / 1000;
@@ -34,7 +44,8 @@ function data(response) {
 		return _.map(_.countBy(languages, _.identity), function(value, key) {
 			return [key, value];
 		});
-	}
+	};
+
 	var requestCallback = function(wsRes) {
 		var str = "";
 		//another chunk of data has been recieved, so append it to `str`
@@ -42,23 +53,37 @@ function data(response) {
 			str += chunk;
 		});
 
-		//the whole response has been recieved, so we just print it out here
+		//the whole response has been received, so we just print it out here
 		wsRes.on('end', function () {
-			var newData = JSON.stringify(processData(JSON.parse(str)));
-			writeResponse(newData);
-			updateCache(newData);
+			if (str == "") {
+				errorCallback("Empty data.");
+			} else {
+				var newData = JSON.stringify(processData(JSON.parse(str)));
+				updateCache(newData);
+				writeResponse(newData);
+			}
 		});
-	}
+	};
+
+	var errorCallback = function(e) {
+		console.error(e);
+		writeResponse(cache.data);
+	};
 
 	function writeResponse(data) {
-		response.writeHead(200, {"Content-Type": "application/json"});
-		response.write(data);
+		if (data == "") {
+			response.writeHead(500, {"Content-Type": "text/plain"});
+			response.write("500 Internal Server Error\nEmpty data")
+		} else {
+			response.writeHead(200, {"Content-Type": "application/json"});
+			response.write(data);
+		}
 		response.end();
 	}
 
 	if (cache.valid < currentTimeInSeconds()) {
 		console.log("cache miss");
-		http.get(dataSource, requestCallback);
+		dataRequestOptions.module.get(dataRequestOptions, requestCallback).on('error', errorCallback);
 	} else {
 		console.log("cache hit");
 		writeResponse(cache.data);
@@ -81,6 +106,26 @@ function onRequest(request, response) {
 	}
 }
 
-http.createServer(onRequest).listen(8888);
+function configurationToOptions(conf) {
+	var parsed = url.parse(conf.dataSource);
+	var module = http;
+	if (parsed.protocol == "https:") {
+		module = https;
+	}
+	return {
+		host: parsed.hostname,
+		module: module,
+		path: parsed.path,
+		port: parsed.port,
+		headers: { "Authorization" : "Bearer " + conf.authToken }
+	};
+}
 
-console.log("Server has started.");
+var configuration = parseArgs(process.argv);
+var dataRequestOptions = configurationToOptions(configuration);
+// console.log("Read configuration: " + JSON.stringify(dataRequestOptions));
+
+http.createServer(onRequest).listen(configuration.port);
+
+console.log("Server has started: http://localhost:" + configuration.port);
+
